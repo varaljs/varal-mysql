477d4f256735025dd053aa82f8d61aed41a7fa0e
dmpty <dmptyy@gmail.com>
2018-01-18 14:53:38 +0800

    1.0.10 Transactions

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ class Mysql {
 
     async query(sqlString, values) {
         let res = undefined;
-        if (this.pool !== undefined)
-            [res] = await this.pool.query(sqlString, values);
+        if (this.pool !== undefined) {
+            const connection = await this.pool.getConnection();
+            [res] = await connection.query(sqlString, values);
+            connection.release();
+        }
         else {
             const connection = await mysql.createConnection(this.options);
             [res] = await connection.query(sqlString, values);
@@ -29,6 +32,18 @@ class Mysql {
         return res;
     }
 
+    async beginTransaction() {
+        let connection = undefined;
+        let pooling = false;
+        if (this.pool !== undefined) {
+            pooling = true;
+            connection = await this.pool.getConnection();
+        }
+        else
+            connection = await mysql.createConnection(this.options);
+        return new Transaction(connection, pooling);
+    }
+
     table(name) {
         return new QueryBuilder(this, name);
     }
@@ -37,6 +52,52 @@ class Mysql {
         const mysql = new Mysql(options);
         return server => server.bind('varal.mysql', mysql);
     }
+
+}
+
+class Transaction {
+
+    constructor(connection, pooling) {
+        this.connection = connection;
+        this.pooling = pooling;
+        this.connection.beginTransaction();
+    }
+
+    async query(sqlString, values) {
+        if (!this.connection)
+            return;
+        let res = undefined;
+        if (this.pooling)
+            [res] = await this.connection.query(sqlString, values);
+        else
+            [res] = await this.connection.query(sqlString, values);
+        if (Array.isArray(res))
+            return new collection(res);
+        return res;
+    }
+
+    table(name) {
+        return new QueryBuilder(this, name);
+    }
+
+    async rollback() {
+        await this.connection.rollback();
+        this.destroy();
+    }
+
+    async commit() {
+        await this.connection.commit();
+        this.destroy();
+    }
+
+    destroy() {
+        if (this.pooling)
+            this.connection.release();
+        else
+            this.connection.destroy();
+        delete this.connection;
+    }
+
 
 }
 
